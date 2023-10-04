51098b917712e534ac3c0f607eaaac7fa38c6fb8
OlenaOP <elena.peredriy@gmail.com>
2023-10-04 23:34:23 +0300

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = event => {
+    console.log(event.target.name);
+    this.setState(prevState => ({
+      [event.target.name]: prevState[event.target.name] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <div className={css.buttonWrap}>
+          <button
+            onClick={event => this.handleIncrement(event)}
+            name="good"
+            type="button"
+          >
+            Good{' '}
+          </button>
+          <button
+            onClick={event => this.handleIncrement(event)}
+            name="neutral"
+            type="button"
+          >
+            Neutral{' '}
+          </button>
+          <button
+            onClick={event => this.handleIncrement(event)}
+            name="bad"
+            type="button"
+          >
+            Bad{' '}
+          </button>
+        </div>
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,10 @@
+export const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
+};
